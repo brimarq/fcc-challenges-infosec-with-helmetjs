5fec84a13677fbe332bca5ddf7eaad2792eff4f9
Brian Marquis <rbmarquis.dev@gmail.com>
2019-02-13 21:02:31 -0600

    require bcrypt

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 'use strict';
-const express     = require('express');
-const bodyParser  = require('body-parser');
-const fccTesting  = require('./freeCodeCamp/fcctesting.js');
-const app         = express();
+const express = require('express');
+const bodyParser = require('body-parser');
+const fccTesting = require('./freeCodeCamp/fcctesting.js');
+const app = express();
+const bcrypt = require('bcrypt');
 
 fccTesting(app); //For FCC testing purposes
 
